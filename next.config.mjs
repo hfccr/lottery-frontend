86f2aa8c797591d77731b941fdbdcd38fb9526d0
hfccr <hfccr@outlook.com>
2024-07-17 21:32:51 +0530

    Add web3 modal config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+      wagmi: false,
+    };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
 
 export default nextConfig;
---
+++ src/context/Web3Modal.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { ReactNode } from "react";
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
+
+// Your WalletConnect Cloud project ID
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID!;
+
+// 2. Set chains
+// const sepolia = {
+//   chainId: 11155111,
+//   name: "Sepolia test network",
+//   currency: "SepoliaETH",
+//   explorerUrl: "https://sepolia.etherscan.io",
+//   rpcUrl: "https://sepolia.drpc.org	",
+// };
+
+const hardhat = {
+  chainId: 31337,
+  name: "Hardhat",
+  currency: "ETH",
+  explorerUrl: "",
+  rpcUrl: "http://127.0.0.1:8545/",
+};
+
+// 3. Create a metadata object
+const metadata = {
+  name: "lottery",
+  description: "Lottery dApp",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+  /*Required*/
+  metadata,
+
+  /*Optional*/
+  enableEIP6963: true, // true by default
+  enableInjected: true, // true by default
+  enableCoinbase: true, // true by default
+  rpcUrl: "...", // used for the Coinbase SDK
+  defaultChainId: 1, // used for the Coinbase SDK
+});
+
+const chains = [hardhat];
+
+export const isValidChain = (chainId: number | undefined) => {
+  return chains.some((chain) => chain.chainId === chainId);
+};
+
+// 5. Create a Web3Modal instance
+createWeb3Modal({
+  ethersConfig,
+  chains,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
+
+export function Web3Modal({ children }: { children: ReactNode }) {
+  return children;
+}
