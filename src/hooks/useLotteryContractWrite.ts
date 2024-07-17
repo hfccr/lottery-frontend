b6b13bde38d1818e68381c91bea7589125efb3b7
hfccr <hfccr@outlook.com>
2024-07-18 01:31:09 +0530

    Add lottery contract write hook using ethers

---
+++ src/hooks/useLotteryContractWrite.ts
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import { ethers, parseEther } from "ethers";
+import Lottery from "@/contracts/Lottery.json";
+import Deployments from "@/contracts/deployedAddresses.json";
+import { BrowserProvider } from "ethers";
+import {
+  useWeb3ModalAccount,
+  useWeb3ModalProvider,
+} from "@web3modal/ethers/react";
+
+interface ContractWriteProps {
+  methodName: string;
+  methodParams: any[];
+  amount: string;
+}
+
+const useLotteryContractWrite = () => {
+  const { Lottery: lotteryContractAddress } = Deployments;
+  const { abi } = Lottery;
+  const { address, chainId, isConnected } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
+  const [provider, setProvider] = useState<BrowserProvider | null>(null);
+  const [writeStatus, setWriteStatus] = useState({
+    fetching: false,
+    error: false,
+    errorMessage: "",
+    success: false,
+    data: null,
+  });
+  useEffect(() => {
+    const init = async () => {
+      try {
+        if (!isConnected || !walletProvider) throw Error("User disconnected");
+        const ethersProvider = new BrowserProvider(walletProvider);
+        setProvider(ethersProvider);
+      } catch (err) {}
+    };
+    init();
+  }, [address, chainId, isConnected]);
+
+  const write = async ({
+    methodName,
+    methodParams,
+    amount,
+  }: ContractWriteProps) => {
+    setWriteStatus({
+      ...writeStatus,
+      fetching: true,
+    });
+    try {
+      if (!provider) {
+        throw Error("Provider not available");
+      }
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(lotteryContractAddress, abi, signer);
+      let amountValue;
+      if (amount !== "") {
+        amountValue = parseEther(amount);
+      }
+      const tx = await contract[methodName](...methodParams, {
+        value: amountValue,
+      });
+      await tx.wait();
+      setWriteStatus({
+        fetching: false,
+        error: false,
+        errorMessage: "",
+        success: true,
+        data: tx,
+      });
+    } catch (err) {
+      console.log(err);
+      setWriteStatus({
+        fetching: false,
+        error: true,
+        errorMessage: (err as Error).message,
+        success: false,
+        data: null,
+      });
+    }
+  };
+
+  return { write, writeStatus };
+};
+
+export default useLotteryContractWrite;
